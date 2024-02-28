3f127f8c239916be319853f55f76581003562e3a
SameerGtm <sameergautam729@gmail.com>
2024-02-28 14:34:18 +0545

    create homepage component to display name and photo

---
+++ src/homePage.js
@@ -0,0 +1,15 @@
+import "./homePage.css";
+const HomePage = () => {
+  return (
+    <div className="profile-container">
+      <img
+        src="/sameer_image.HEIC"
+        alt="Sameer Gautam"
+        className="profile-image"
+      />
+      <h1>Sameer Gautam</h1>
+      <h3>Computer Engineering Student</h3>
+    </div>
+  );
+};
+export default HomePage;
